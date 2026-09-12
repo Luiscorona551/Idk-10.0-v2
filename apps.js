bfd3e8446766434236619fa8864dfe654a969eee
Luiscorona551 <Coronaluis2012@icloud.com>
2026-09-12 00:45:38 -0500

    Use stable N64 emulator fallback

--- apps.js
+++ apps.js
@@ -105,6 +105,7 @@ function gameIconURL(path) {
 
 function gameSourceWithBase(html) {
   html = html.replaceAll('https://cdn.jsdelivr.net/gh/bubblfan/emu@master/', GAME_EMULATOR_CDN);
+  html = html.replace(/EJS_core\s*=\s*["']parallel_n64["']/g, 'EJS_core = "mupen64plus_next"');
   if (/\bEJS_(?:pathtodata|core)\b/i.test(html) && !/EJS_DEBUG_XX\s*=/i.test(html)) {
     const debug = '<script>window.EJS_DEBUG_XX = true;</script>';
     if (/<head\b/i.test(html)) html = html.replace(/<head\b[^>]*>/i, match => `${match}${debug}`);
